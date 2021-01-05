32ff95a9bf5a3a0ffd556319a0348d7ab8d18648
einar <einarlop0@gmail.com>
2021-01-05 09:30:19 -0600

    Create Oproject and Tag, login and register

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-var express = require('express');
-require('dotenv').config()  // to load the values specified in .env
+var express = require("express");
+require("dotenv").config(); // to load the values specified in .env
+var bodyParser = require("body-parser");
 
 var app = express();
-
+app.use(bodyParser.json());
 var port = process.env.PORT || 3000;
 
 // Require Router modules
-var indexRouter = require('./routes/index');
-var projectsRouter = require('./routes/projects');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var projectsRouter = require("./routes/projects");
+var usersRouter = require("./routes/users");
 
 // Mongoose Setup
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const uri = process.env.CONNECTIONSTRING;
-mongoose.connect(uri, {useNewUrlParser:true, useUnifiedTopology: true});
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
 const db = mongoose.connection;
-db.on('error', console.error.bind(console.error, 'MongoDB Connection Error'));
+db.on("error", console.error.bind(console.error, "MongoDB Connection Error"));
 console.log("Connection to mongoDB succesful");
 
 // Middleware libraries for request handling
-app.use('/', indexRouter);
-app.use('/projects', projectsRouter);
-app.use('/users', usersRouter);
-app.use(express.json())
+app.use("/", indexRouter);
+app.use("/projects", projectsRouter);
+app.use("/users", usersRouter);
+app.use(express.json());
 
-app.listen(port, ()=> console.log('Express server up and running: ' + port));
+app.listen(port, () => console.log("Express server up and running: " + port));
--- controllers/tagController.js
+++ controllers/tagController.js
@@ -1,36 +1,47 @@
-const Tag = require('../models/tag');
-
+const Tag = require("../models/tag");
 
 // Route handler definitions
 
 // GET actions
-exports.getList = function(req, res) {
-    res.send("Tag list here.");
-}
-exports.getDetail = function(req, res) {
-    res.send("Tag detail of: " + req.params.id);
-}
+exports.getList = function (req, res) {
+  res.send("Tag list here.");
+};
+exports.getDetail = function (req, res) {
+  res.send("Tag detail of: " + req.params.id);
+};
 
-exports.createForm = function(req, res) {
-    res.send("Creation form for Tag");
-}
-exports.deleteForm = function(req, res) {
-    res.send("Deletion form for Tag");
-}
-exports.updateForm = function(req, res) {
-    res.send("Update form for Tag");
-}
-
+exports.createForm = function (req, res) {
+  res.send("Creation form for Tag");
+};
+exports.deleteForm = function (req, res) {
+  res.send("Deletion form for Tag");
+};
+exports.updateForm = function (req, res) {
+  res.send("Update form for Tag");
+};
 
 // POST actions
-exports.createTag = function(req, res) {
-    res.send("Create Tag not implemented");
-}
+exports.createTag = function (req, res) {
+  const name = req.body.name;
+  const oprojects = req.body.oprojects;
+  const sprojects = req.body.sprojects;
 
-exports.deleteTag = function(req, res) {
-    res.send("Delete Tag not implemented");
-}
+  let tag = new Tag({
+    name,
+    oprojects,
+    sprojects,
+  });
 
-exports.updateTag = function(req, res) {
-    res.send("Update Tag not implemented");
-}
+  tag
+    .save()
+    .then(() => res.json("Tag added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+};
+
+exports.deleteTag = function (req, res) {
+  res.send("Delete Tag not implemented");
+};
+
+exports.updateTag = function (req, res) {
+  res.send("Update Tag not implemented");
+};
