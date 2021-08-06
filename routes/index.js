fcd030c10217c682349372aa22210b9776448314
Eric Chao <ericjardon@hotmail.com>
2021-08-06 03:35:42 -0500

    Fix /about route content, refactor comments

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,24 @@ const router = express.Router();
 
 /* Home page GET */
 router.get("/", function (req, res) {
-  //res.redirect('/projects');
   res.send("WELCOME TO GEEB3 API");
 });
 
 router.get("/about", function (req, res) {
-  res.send(
-    ```
-    This is the about page. We explain what how to use the GEEB3API, what it offers,\n
-    And what it can do for you as a developer.
-    \n
-    URL PATHS:
-    \t'about/users' => users tutorial\n
-    \t'about/sprojects' => sprojects tutorial\n
-    \t'about/oprojects' => oprojects tutorial\n
-    \t'about/tags' => tags tutorial\n
-    \t'about/skills' => skills tutorial\n
-    \t'about/applicants' => applicants tutorial\n
-    \t'about/feedback' => feedback comments tutorial\n
-    ```
-  );
+  let content = `
+  This is the about page. We explain what how to use the GEEB3API, what it offers,\n
+  And what it can do for you as a developer.
+  \n
+  URL PATHS:
+  \t'about/users' => users tutorial\n
+  \t'about/sprojects' => sprojects tutorial\n
+  \t'about/oprojects' => oprojects tutorial\n
+  \t'about/tags' => tags tutorial\n
+  \t'about/skills' => skills tutorial\n
+  \t'about/applicants' => applicants tutorial\n
+  \t'about/feedback' => feedback comments tutorial\n
+  `;
+  res.send(content.toString());
 });
 
 
